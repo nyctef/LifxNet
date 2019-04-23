fc08a20982a012f767ac13e5df268d562ff22756
Nyctef <nyctef@nyctef.com>
2019-04-23 13:17:14 +0100

    header serialization is slightly wrong
     try writing a test with a correct packet from wireshark

---
+++ src/LifxNet.Tests/Protocol/MessageSerializationTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LifxNet.Tests.Protocol
+{
+    public class MessageSerializationTests
+    {
+        [Test]
+        public void GetService()
+        {
+            var expected = "24 00 00 34 4c e5 c1 06 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 02 00 00 00";
+
+            var message = LifxMessage.CreateBroadcast(new GetServiceRequest(), 113370444, true, false, 0);
+            var actual = FormatBytes(GetBytes(message));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private byte[] GetBytes(LifxMessage message)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                message.WriteToStream(memoryStream);
+
+                return memoryStream.ToArray();
+            }
+        }
+
+        private string FormatBytes(byte[] bytes)
+        {
+            return string.Join(" ", bytes.Select(b => b.ToString("X2")).ToArray()).ToLower();
+        }
+    }
+}
