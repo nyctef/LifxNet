c58e09bece5fa07d7cb7835e9836afcfa3c13277
Nyctef <nyctef@nyctef.com>
2019-04-23 14:01:25 +0100

    remove SampleApps and port commandline to netcoreapp

--- src/lifxctl/Program.cs
+++ src/lifxctl/Program.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LifxNet;
+﻿using LifxNet;
+using System;
 
-namespace SampleApp.NET462
+namespace lifxctl
 {
     class Program
     {
         static LifxNet.LifxClient client;
         static void Main(string[] args)
         {
-            var task = LifxNet.LifxClient.CreateAsync();
+            var task = LifxNet.LifxClient.CreateAsync(new TraceLogger());
             task.Wait();
             client = task.Result;
             client.DeviceDiscovered += Client_DeviceDiscovered;
@@ -28,8 +24,8 @@ namespace SampleApp.NET462
 
         private static async void Client_DeviceDiscovered(object sender, LifxClient.DeviceDiscoveryEventArgs e)
         {
-            Console.WriteLine($"Device {e.Device.MacAddressName} found @ {e.Device.HostName}");
-            var version = await client.GetDeviceVersionAsync(e.Device);
+            Console.WriteLine($"Device {e.Device.MacAddressName} found @ {e.Device.Endpoint}");
+            //var version = await client.GetDeviceVersionAsync(e.Device);
             //var label = await client.GetDeviceLabelAsync(e.Device);
             var state = await client.GetLightStateAsync(e.Device as LightBulb);
             Console.WriteLine($"{state.Label}\n\tIs on: {state.IsOn}\n\tHue: {state.Hue}\n\tSaturation: {state.Saturation}\n\tBrightness: {state.Brightness}\n\tTemperature: {state.Kelvin}");
@@ -37,4 +33,5 @@ namespace SampleApp.NET462
             await client.TurnBulbOffAsync(e.Device as LightBulb, TimeSpan.FromMilliseconds(0));
         }
     }
+
 }
