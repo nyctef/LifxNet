750544ead62acafe888701cafea3f18fcc98ef7d
Nyctef <nyctef@nyctef.com>
2019-04-24 16:54:15 +0100

    start experimenting with a more comprehensive commandline

--- src/lifxctl/Program.cs
+++ src/lifxctl/Program.cs
@@ -1,37 +1,151 @@
 ﻿using LifxNet;
 using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace lifxctl
 {
     class Program
     {
-        static LifxNet.LifxClient client;
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            var task = LifxNet.LifxClient.CreateAsync(new TraceLogger());
-            task.Wait();
-            client = task.Result;
-            client.DeviceDiscovered += Client_DeviceDiscovered;
-            client.DeviceLost += Client_DeviceLost;
+            var rootCommand = new RootCommand();
+
+            var discoverCommand = new Command("discover")
+            {
+                Handler = CommandHandler.Create(RunDiscover)
+            };
+            rootCommand.AddCommand(discoverCommand);
+
+            var lightCommand = new Command("light");
+            Option deviceSpec = new Option("--device-spec", argument: new Argument<string>() { Arity = ArgumentArity.ExactlyOne });
+            ValidateSymbol checkDeviceSpec = r =>
+               {
+                // apparently we have to do this validation manually at the moment (https://github.com/dotnet/command-line-api/issues/484)
+                if (r.Children["device-spec"] is null)
+                   {
+                       return "device-spec is required";
+                   }
+                   else
+                   {
+                       return null;
+                   }
+               };
+
+            Command lightOnCommand = new Command("on") { Handler = CommandHandler.Create<string>(RunLightOn), };
+            lightOnCommand.AddOption(deviceSpec);
+            lightOnCommand.Argument.AddValidator(checkDeviceSpec);
+            Command lightOffCommand = new Command("off") { Handler = CommandHandler.Create<string>(RunLightOff), };
+            lightOffCommand.AddOption(deviceSpec);
+            lightOffCommand.Argument.AddValidator(checkDeviceSpec);
+
+            lightCommand.AddCommand(lightOnCommand);
+            lightCommand.AddCommand(lightOffCommand);
+
+            rootCommand.AddCommand(lightCommand);
+
+
+            var parser = new CommandLineBuilder(rootCommand)
+                .UseDefaults()
+                .UseParseDirective()
+                .EnablePositionalOptions()
+                .Build();
+
+            return await parser.InvokeAsync(parser.Parse(args));
+        }
+
+        private static async Task<int> RunDiscover()
+        {
+            var client = await LifxClient.CreateAsync(new TraceLogger());
+
+            client.DeviceDiscovered += (o,e) =>
+            {
+                Console.WriteLine($"{e.Device.MacAddressName}@{e.Device.Endpoint.Address}:{e.Device.Endpoint.Port}");
+                //Console.WriteLine($"Device {e.Device.MacAddressName} found @ {e.Device.Endpoint}");
+                //var version = await client.GetDeviceVersionAsync(e.Device);
+                //var label = await client.GetDeviceLabelAsync(e.Device);
+                //var state = await client.GetLightStateAsync(e.Device as LightBulb);
+                //Console.WriteLine($"{state.Label}\n\tIs on: {state.IsOn}\n\tHue: {state.Hue}\n\tSaturation: {state.Saturation}\n\tBrightness: {state.Brightness}\n\tTemperature: {state.Kelvin}");
+
+            };
+
             client.StartDeviceDiscovery();
-            Console.ReadKey();
+
+            // give some time for discovery to complete
+            await Task.Delay(1000);
+
+            return 0;
         }
 
-        private static void Client_DeviceLost(object sender, LifxClient.DeviceDiscoveryEventArgs e)
+        private static async Task<int> RunLightOn(string deviceSpec)
         {
-            Console.WriteLine("Device lost");
+            var parsedSpec = DeviceSpec.Parse(deviceSpec);
+            var bulb = LightBulb.Create(parsedSpec.IPEndPoint, parsedSpec.MacAddress, service:1);
+
+            var client = await LifxClient.CreateAsync(new TraceLogger());
+
+            await client.TurnBulbOnAsync(bulb, TimeSpan.FromMilliseconds(0));
+
+            return 0;
         }
 
-        private static async void Client_DeviceDiscovered(object sender, LifxClient.DeviceDiscoveryEventArgs e)
+        private static async Task<int> RunLightOff(string deviceSpec)
         {
-            Console.WriteLine($"Device {e.Device.MacAddressName} found @ {e.Device.Endpoint}");
-            //var version = await client.GetDeviceVersionAsync(e.Device);
-            //var label = await client.GetDeviceLabelAsync(e.Device);
-            var state = await client.GetLightStateAsync(e.Device as LightBulb);
-            Console.WriteLine($"{state.Label}\n\tIs on: {state.IsOn}\n\tHue: {state.Hue}\n\tSaturation: {state.Saturation}\n\tBrightness: {state.Brightness}\n\tTemperature: {state.Kelvin}");
+            var parsedSpec = DeviceSpec.Parse(deviceSpec);
+            var bulb = LightBulb.Create(parsedSpec.IPEndPoint, parsedSpec.MacAddress, service: 1);
 
-            await client.TurnBulbOffAsync(e.Device as LightBulb, TimeSpan.FromMilliseconds(0));
+            var client = await LifxClient.CreateAsync(new TraceLogger());
+
+            await client.TurnBulbOffAsync(bulb, TimeSpan.FromMilliseconds(0));
+
+            return 0;
         }
     }
 
+    internal class DeviceSpec
+    {
+        public readonly IPEndPoint IPEndPoint;
+        public readonly PhysicalAddress MacAddress;
+
+        public DeviceSpec(IPEndPoint iPEndPoint, PhysicalAddress physicalAddress)
+        {
+            IPEndPoint = iPEndPoint;
+            MacAddress = physicalAddress;
+        }
+
+        internal static DeviceSpec Parse(string deviceSpec)
+        {
+            var regex = new Regex(@"
+^
+(?<mac>(?:\w\w\:?)+)
+\@
+(?<ip>(?:\d+\.?)+)
+\:
+(?<port>\d+)
+$
+
+", RegexOptions.IgnorePatternWhitespace);
+
+            var match = regex.Match(deviceSpec);
+
+            if (!match.Success)
+            {
+                throw new ArgumentException($"Failed to parse spec {deviceSpec}");
+            }
+
+            string ip = match.Groups["ip"].Value;
+            string port = match.Groups["port"].Value;
+            string mac = match.Groups["mac"].Value;
+
+            Console.WriteLine($"{ip} / {port} / {mac}");
+
+            return new DeviceSpec(new IPEndPoint(IPAddress.Parse(ip), int.Parse(port)), PhysicalAddress.Parse(mac.ToUpper().Replace(":", "")));
+        }
+    }
 }
